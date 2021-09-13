9bb1ec40dc0d42eb50461c9d3818edc1ec4062eb
董亚威 <2594810013@qq.com>
2021-09-13 19:55:20 +0800

    更新readme

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+//http包响应报文结构
+//type Response struct {
+//	Status     string  //响应状态
+//	StatusCode int     //响应状态码
+//	Proto      string  //协议类型
+//	ProtoMajor int     //协议的主版本号
+//	ProtoMinor int     //协议的副版本号
+//	Header http.Header
+//	Body io.ReadCloser
+//	ContentLength int64
+//	TransferEncoding []string
+//	Close bool
+//	Trailer http.Header
+//	Request http.Request
+//	TLS *tls.ConnectionState
+//}
+func main() {
+
+}
+
+func GetHTTP() {
+
+}
